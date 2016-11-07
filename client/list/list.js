150442a07e47d0d240dc542a147811fce6327e51
Patrick Truong <pahtruon@gmail.com>
2016-11-07 15:52:04 -0800

    (feat) add edit to save edits to list and update map

--- client/list/list.js
+++ client/list/list.js
@@ -1,5 +1,5 @@
 angular.module('cordsList', [])
-  .controller('cordsListController', ['$scope', 'data', function($scope, data) {
+  .controller('cordsListController', ['$scope', 'data', '$rootScope', function($scope, data, $rootScope) {
     $scope.cordslist;
 
     // Loads data from pct-data.json into local storage and set variable to it for data binding
@@ -17,16 +17,26 @@ angular.module('cordsList', [])
         })
     };
 
+    // Saves edit to local storage and updates map
+    $scope.edit = (cords, index) => {
+      // Use this to parse the input strings from the edits
+      for (let i = 0; i < 3; i++) {
+        cords[i] = JSON.parse(cords[i]);
+      }
+      window.localStorage.setItem('listData', JSON.stringify($scope.cordslist));
+      $rootScope.$broadcast('updateMap');
+    };
+
     // Deletes coordinate from list, updates local stroage and broadcast to map controller tp update
     $scope.delete = (index) => {
       $scope.cordslist.splice(index, 1);
       window.localStorage.setItem('listData', JSON.stringify($scope.cordslist));
+      $rootScope.$broadcast('updateMap');
     };
 
     // Event listener for any updates to local storage
     $rootScope.$on('updateList', () => {
       $scope.cordslist = JSON.parse(window.localStorage.getItem('listData'));
-      $rootScope.$broadcast('updateMap');
     });
 
   }])
