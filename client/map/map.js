61064b99dc73d435ff256a7306e07dd69a1bd98e
Patrick Truong <pahtruon@gmail.com>
2016-11-07 15:31:45 -0800

    (feat) add functions to initalize map and add cords to scope array

--- client/map/map.js
+++ client/map/map.js
@@ -4,5 +4,19 @@ angular.module('map', [])
     mc.postitions = [];
     mc.map;
 
+    // Initialize map to mc.map for future use
+    const getMap = () => {
+      NgMap.getMap('ng-map')
+        .then((map) => {
+          mc.map = map;
+          console.log('center' , map.getCenter());
+          console.log('markers', map.markers);
+        })
+    };
+
+    mc.$onInit = () => {
+      getMap();
+      mc.postitions = JSON.parse(window.localStorage.getItem('listData'));
+    };
 
   }])
